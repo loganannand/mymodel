e31f0d817523c446e50680a2dd9c8d7f122c2240
Logan Annand <39375006+loganannand@users.noreply.github.com>
2022-02-09 20:56:01 +1300

    Update mymodel.py

--- mymodel.py
+++ mymodel.py
@@ -15,8 +15,7 @@ defaultclock.dt = 0.001 * ms
 
 I_off = 0 * mV
 I_on = 10 * mV
-I_app_start = 20 * ms
-
+I_app_start = 100 * ms
 
 # ESSneuron_type = ['regular spiking', 'fast spiking', 'regular spiking', 'regular spiking']
 '''ESSneuron_type = ['fast spiking']
@@ -29,7 +28,7 @@ abcd_neurons = {'regular spiking': [0.065 / ms, 0.2, -65 * mV, 8 * mV],
                 'intrinsically bursting': [0.02 / ms, 0.2, -55 * mV, 4 * mV],
                 'chattering': [0.02 / ms, 0.2, -50 * mV, 2 * mV],
 
-                'fast spiking': [0.08 / ms, 0.2, -65 * mV, 2 * mV],
+                'fast spiking': [0.4 / ms, 0.2, -65 * mV, 2 * mV],
                 'low-threshold spiking': [0.02 / ms, 0.25, -65 * mV, 2 * mV],
 
                 'thalamo-cortical': [0.02 / ms, 0.25, -65 * mV, 0.05 * mV],
@@ -53,66 +52,82 @@ IZeqs = '''
 
 reset_eqs = 'v = c; u = u + d'
 
-#I_app_fn = TimedArray([I_off, I_on], dt=I_app_start) # Replace first argument with array defining spikes. Was '[I_off, I_on]'
-I_array = np.zeros(250)
-pulses = list(range(20, 230, 15))
-I = 20
+I_app_fn = TimedArray([I_off, I_on], dt=I_app_start)
+
+'''I_array = np.zeros(800)  # 250ms run time
+pulses = list(range(50, 751))
+I = 10
 for i in pulses:
     I_array[i] = I
+I_app_fn = I_input = TimedArray(I_array * mV, dt=1 * ms)'''
 
-I_app_fn = I_input = TimedArray(I_array * mV, dt=1 * ms)
+'''
+I = 10
+I_array = np.zeros(360)
+stim_range = list(range(170, 290))
+for i in stim_range:
+    I_array[i] = I
+#I_app_fn = TimedArray(I_array * mV, dt=1 * ms)'''
+
 N = 1  # Number of neurons
 G = NeuronGroup(N, IZeqs, threshold='v > v_th', reset=reset_eqs, method='euler')
 G.v = c
 G.u = 0 * mV
 
-'''G.v[1] = -65*mV # Initial value of neuron 2?'''
 
 # Freq vs input code
 '''inputs = list(range(10, 110, 10)) *mV # Range of input currents from 10mV to 80mV
 frequency = []
 '''
 # Synapses
-'''S = Synapses(G, G, 'w : 1', on_pre='v_post += 0.2*mV')
-S.connect(i=0, j=1)
-S.delay = 5*ms
-'''
+'''synapse = Synapses(G, neurongroup2,
+                   model='''s: volt''',
+                   on_pre='v += s')
+synapse.connect(i=[0, 1], j=[0, 1])
+synapse.s = 100. * mV'''
+
 
 # Monitors
 M = StateMonitor(G, variables=['v', 'I_app', 'u'], record=True)
 spikemon = SpikeMonitor(G)
 run(duration)
-'''frequency.append((len(spikemon)/230)*1000)
-print(frequency)'''
-#frequencies.append(round((len(spikemon) / 230) * 1000))
+
 # Plotting monitors
-f, axs = plt.subplots(3, 1, figsize=(8, 6), gridspec_kw={'height_ratios': [3, 1, 1]})
+f, axs = plt.subplots(2, 1, figsize=(8, 6), gridspec_kw={'height_ratios': [3, 1]})
 
 axs[0].plot(M.t / ms, M.v[0] / mV, 'k')  # Voltage plot
 axs[1].plot(M.t / ms, M[0].I_app / mV, 'k')  # Input current plot
-axs[2].plot(M.t / ms, M.u[0] / mV, 'k')  # Reset variable plot
+# axs[2].plot(M.t / ms, M.u[0] / mV, 'k')  # Reset variable plot
+axs[0].plot(M.t / ms, M.v[1] / mV, 'r', linestyle='dashed')
 
-# axs[0].plot(M.t / ms, M.v[1] / mV, color='r', linestyle='dashed') # Plot second RS neuron voltage
-
-axs[0].set_ylabel('Voltage [mV]')
-axs[0].set_title('IZH MPE neuron', fontsize=20)
-axs[1].set_ylabel('Input current [mV] ')  # axs[2].set_ylabel('Reset [u]')
-axs[2].set_xlabel('Time [ms]')
+# axs[0].set_ylabel('Voltage [mV]')
+axs[0].set_title('FS', fontsize=18)
+# axs[1].set_ylabel('Input current [mV] ')  # axs[2].set_ylabel('Reset [u]')
+# axs[1].set_xlabel('Time [ms]')
+'''axs[0].set_xlim([160,300])
+axs[1].set_xlim([160,300])'''
+# axs[2].set_xlabel('Time [ms]')
 axs[0].spines['right'].set_visible(False)
 axs[0].spines['top'].set_visible(False)
 axs[0].spines['bottom'].set_visible(False)
+axs[0].spines['left'].set_visible(False)
 axs[1].spines['right'].set_visible(False)
 axs[1].spines['top'].set_visible(False)
 axs[1].spines['bottom'].set_visible(False)
-axs[2].spines['top'].set_visible(False)
-axs[2].spines['right'].set_visible(False)
+axs[1].spines['left'].set_visible(False)
+
+# axs[1].spines['bottom'].set_visible(False)
+# axs[2].spines['top'].set_visible(False)
+# axs[2].spines['right'].set_visible(False)
 
 axs[0].set_xticks([])
 axs[1].set_xticks([])
+axs[0].set_yticks([])
+axs[1].set_yticks([])
 
 plt.show()
 
 print('spike times [ms]', spikemon.t)
 print(len(spikemon.t))
 
-#print(frequencies)
+# print(frequencies)
